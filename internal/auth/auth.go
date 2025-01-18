f65eda8aae47f94936f0208ac1fadd3c80a274e7
nindgabeet <wenming.soh@gmail.com>
2025-01-18 08:46:15 +0800

    worked on jwt functionality

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (hashedPassword string, err error) {
 	passwordBytes := []byte(password)
@@ -16,4 +23,48 @@ func CheckPasswordHash(password, hash string) (err error) {
 	hashBytes := []byte(hash)
 	hashErr := bcrypt.CompareHashAndPassword(hashBytes, passwordBytes)
 	return hashErr
+}
+
+func MakeJWTWithClaims(uuid uuid.UUID, secret string, expirationTime time.Duration) (string, error) {
+	expiration := time.Now().Add(expirationTime)
+	uuidString := uuid.String()
+	if uuidString == "" {
+		return "", errors.New("uuid passed in could not be parsed to string")
+	}
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(expiration),
+		Subject:   uuid.String(),
+	}
+
+	var signMethod jwt.SigningMethod = jwt.SigningMethodHS256
+
+	token := jwt.NewWithClaims(signMethod, claims)
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString, secret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(*jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if !token.Valid {
+		return uuid.Nil, err
+	}
+	if claims.ExpiresAt.Time.Before(time.Now()) {
+		return uuid.Nil, errors.New("token has expired")
+	}
+	parseUUID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return parseUUID, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,7 +2,12 @@ package auth
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func TestHashPassword(t *testing.T) {
@@ -62,4 +67,87 @@ func TestCheckPasswordHash(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestMakeJWTAndParse(t *testing.T) {
+	uuid := uuid.New()
+	tokenString := getTokenString(t, uuid, 3*time.Second, "this is my secret")
+	claims := jwt.RegisteredClaims{}
+	parser := jwt.NewParser()
+	parser.ParseUnverified(tokenString, &claims)
+	val := reflect.ValueOf(claims)
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Type().Field(i)
+		fieldValue := val.Field(i)
+		fmt.Printf("%s: %v\n", field.Name, fieldValue)
+	}
+}
+
+func TestParseTokenString(t *testing.T) {
+	type testStruct struct {
+		name               string
+		tokenDuration      time.Duration
+		secret             string
+		isTestTamperSecret bool
+		isErrExpected      bool
+	}
+
+	tests := []testStruct{
+		{
+			"test token should pass",
+			3 * time.Second,
+			"this is the secret",
+			false,
+			false,
+		},
+		{
+			"test tamper secret",
+			3 * time.Second,
+			"this is the secret",
+			true,
+			false,
+		},
+		{
+			"test token expired",
+			0 * time.Second,
+			"this is the secret",
+			false,
+			true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			uuid := uuid.New()
+			tokenString := getTokenString(t, uuid, test.tokenDuration, test.secret)
+			switch test.isErrExpected {
+			case false:
+				returnedUUID, err := ValidateJWT(tokenString, test.secret)
+				if err != nil {
+					t.Errorf("didn't expect error, got %v\n", err)
+				}
+				if uuid != returnedUUID {
+					t.Errorf("got uuid: %v\nwant uuid: %v", returnedUUID, uuid)
+				}
+			case true:
+
+				if test.isTestTamperSecret {
+					test.secret = fmt.Sprintf("%s - add fail", test.secret)
+				}
+				_, err := ValidateJWT(tokenString, test.secret)
+				if err == nil {
+					t.Errorf("expected error, didn't get one")
+				}
+
+			}
+
+		})
+	}
+}
+
+func getTokenString(t *testing.T, uuid uuid.UUID, duration time.Duration, secret string) string {
+	tokenString, err := MakeJWTWithClaims(uuid, secret, duration)
+	if err != nil {
+		t.Errorf("didn't expect error, got %v", err)
+	}
+	return tokenString
 }
